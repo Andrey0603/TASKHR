575ee91cf65cd973bf56911714a492b1b3045396
Andrey <cliff_06@bk.ru>
2024-02-04 15:17:12 +0500

    написали код в файле Main

--- Main.java
+++ Main.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        
-    }
     
+    public static void main(String[] args) {
+        String[][] inputArrays = {
+                {"Hello", "2", "world", ":-)"},
+                {"1234", "1567", "-2", "computer science"},
+                {"Russia", "Denmark", "Kazan"}
+        };
+
+        for (String[] originalArray : inputArrays) {
+            String[] newArray = formNewArray(originalArray);
+            printArray(newArray);
+        }
+    }
+
+    public static String[] formNewArray(String[] originalArray) {
+        int count = 0;
+        for (String str : originalArray) {
+            if (str.length() <= 3) {
+                count++;
+            }
+        }
+
+        String[] newArray = new String[count];
+        int index = 0;
+        for (String str : originalArray) {
+            if (str.length() <= 3) {
+                newArray[index] = str;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+
+    public static void printArray(String[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("\"" + arr[i] + "\"");
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
 }
